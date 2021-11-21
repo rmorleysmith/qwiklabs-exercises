f009a35a44531f89c6cd2eaf0978756fc4db421d
Richard Morley-Smith <41342522+ItsJustRizzy@users.noreply.github.com>
2021-11-21 21:28:48 +0000

    Add files via upload

---
+++ automating-infrastructure-on-google-cloud-with-terraform/interact-with-terraform-modules/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "qwiklabs-gcp-04-864a9055d684"
+}
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "qwiklabs-gcp-04-864a9055d684"
+}
